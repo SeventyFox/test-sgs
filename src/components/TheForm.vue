bf266641ea557e4557430b7c3043d27abf70d817
SeventyFox <smirnova.appolinaria@gmail.com>
2023-01-27 13:34:39 +0300

    initial commit

---
+++ src/components/TheForm.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <p>Город:</p>
+    <select v-model="model.city" ref="citySelect">
+      <option disabled :value="null">Выберите город</option>
+      <option :selected="isSelectedCity" v-for="(city, idx) in cities" :value="city.id" :key="idx">
+        {{ city.name }}
+      </option>
+    </select>
+  </div>
+  <div>
+    <p>Цех:</p>
+    <select v-model="model.department" ref="departmentSelect" :disabled="isDepSelDisabled">
+      <option disabled :value="null">Выберите цех</option>
+      <option v-for="(department, idx) in deps" :value="department.id" :key="idx">{{ department.name }}</option>
+    </select>
+  </div>
+  <div>
+    <p>Сотрудник:</p>
+    <select v-model="model.employee" ref="employeeSelect" :disabled="isEmpSelDisabled">
+      <option disabled :value="null">Выберите сотрудника</option>
+      <option v-for="(employee, idx) in emp" :value="employee.id" :key="idx">{{ employee.name }}</option>
+    </select>
+  </div>
+  <div>
+    <p>Бригада:</p>
+    <select v-model="model.brigade" :disabled="isEmpSelDisabled">
+      <option disabled :value="null">Выберите бригаду</option>
+      <option value="0">I</option>
+      <option value="1">II</option>
+      <option value="3">III</option>
+    </select>
+  </div>
+  <div>
+    <p>Смена:</p>
+    <select v-model="model.shift" :disabled="isEmpSelDisabled">
+      <option disabled :value="null">Выберите смену</option>
+      <option value="0">1</option>
+      <option value="1">2</option>
+    </select>
+  </div>
+  <button ref="submitButton" @click="setCookie">Выбрать</button>
+</template>
+
+<script setup>
+  import cities from "@/data/cities.json";
+  import departments from "@/data/departments.json";
+  import employees from "@/data/employees.json";
+
+  import { ref, watch } from "vue";
+
+  const deps = ref();
+  const emp = ref();
+  const citySelect = ref();
+  const departmentSelect = ref();
+  const employeeSelect = ref();
+  const isDepSelDisabled = ref(true);
+  const isEmpSelDisabled = ref(true);
+  const isSelectedCity = ref();
+  const model = ref({
+    city: null,
+    department: null,
+    employee: null,
+    brigade: null,
+    shift: null,
+  });
+  watch(model, () => {}, { deep: true });
+  watch(
+    () => model.value.city,
+    (newValue) => {
+      const _deps = departments.filter(({ cityId }) => newValue === cityId);
+      deps.value = _deps;
+      isDepSelDisabled.value = false;
+      model.value.department = null;
+    }
+  );
+  watch(
+    () => model.value.department,
+    (newValue) => {
+      const _emp = employees.filter((employee) => newValue === employee.departmentId);
+      emp.value = _emp;
+      isEmpSelDisabled.value = false;
+      model.value.employee = null;
+    }
+  );
+  const setCookie = () => {
+    document.cookie = `formData=${JSON.stringify(model.value)}`;
+  };
+</script>
